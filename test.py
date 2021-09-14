ac8c859eedae2527872623df0462b6df55cf65f0
guru@users.noreply.github.com <student@bchd.7a8817cf-c3f5-4b8a-935c-7e56fbff5efd>
2021-09-14 12:12:40 +0000

    IF BLANK

---
+++ test.py
@@ -0,0 +1,9 @@
+#i/user/bin/env python3
+
+file=open("lines.txt", "r")
+lines=file.readlines()
+for line in lines:
+    if line=="\n":
+        print("Empty Line")
+    else:
+        print("Non-empty line")
