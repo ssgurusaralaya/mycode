f802d22297a47a252d6ee0f8ecff0db9dc660245
Gnana Guru S S <ssgurusaralaya@gmail.com>
2022-01-21 13:50:14 +0530

    deleted:    5g_research/sdn_network.txt 	deleted:    5g_research/sdn_network.txt.copy 	deleted:    5g_research_backup/sdn_network.txt 	deleted:    5g_research_backup/sdn_network.txt.copy 	deleted:    Agnostic_nw_Solution.yml

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-file=open("lines.txt", "r")
-lines=file.readlines()
-for line in lines:
-    if line=="\n":
-        print("Empty Line")
-    else:
-        print("Non-empty line")
