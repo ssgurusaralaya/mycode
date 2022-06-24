b7faa4a982b3890248b3f846355d6520121e050c
Gnana Guru S S <ssgurusaralaya@gmail.com>
2022-06-24 19:06:01 +0530

    update_Readme

--- iss/ride_issnew.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Alta3 || Tracking ISS"""
-import requests
-MAJORTOM = 'http://api.open-notify.org/astros.json'
-def main():
-    helmetson= requests.get(MAJORTOM).json()
-    print(helmetson)
-main()
--- pyhello.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-print("Hello again my friend! How are you?")
- 
-myfile = open("/home/student/stopper.txt", 'a')
-myfile.close()
-
